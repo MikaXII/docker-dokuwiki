286f7aa41de1e93e7b04abd5e94c63974ef01f6d
MikaXII <mikael.joubert@supinfo.com>
2016-09-23 11:27:23 +0200

    Update dokuwiki.sh

--- dokuwiki.sh
+++ dokuwiki.sh
@@ -4,6 +4,7 @@ if [ ! -e ".stamp_migration" ]; then
   date > last_import.log
   /usr/local/bin/migration.sh
   touch .stamp_migration
+  echo Migration_OK
 fi
 /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
 
